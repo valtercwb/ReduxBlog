ad1c93e8a111ab0a842359e046a11406423b48a9
Franco <valterpassion@hotmail.com>
2017-12-22 16:35:52 -0200

    updates

--- src/actions/index.js
+++ src/actions/index.js
@@ -29,8 +29,9 @@ export function createPost(values, callback){
 
 export function fetchPost(id){
     const request = axios.get(`${ROOT_URL}/posts/${ id }${API_KEY}`);
+    console.log(request);
     return{
         type: FETCH_POST,
         payload: request
-    }
+    };
 }
--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -5,22 +5,35 @@ import { fetchPosts, fetchPost } from '../actions';
 class PostsShow extends Component{
 
     componentDidMount(){
-        this.props.match.params.id;
-        this.props.fetchPost();
+        //the params gets all the wildcards values from the url
+        const { id } = this.props.match.params;
+        this.props.fetchPost(id);
     }
 
     render(){
-    return (
+        //posts[this.props.match.params.id];  the post
+    const { post } = this.props;
+
+        if(!post){
+            return <div>Loading...</div>;
+        }
+
+        return (
     <div>
-        Posts show!
+       <h3> { post.title } </h3>
+       <h6>Categories: { post.categories }</h6>
+       <p> { post.content } </p>
     </div>
         );
 
     };
 }
 
-function mapStateToProps({posts}){
-return {  };
+//                      the list  onwProps is the
+// props object that is headed or going to this component
+function mapStateToProps({ posts }, ownProps){
+    console.log({ post : posts[ownProps.match.params.id] });
+return { post : posts[ownProps.match.params.id] };
 }
 
-export default connect(null,{ fetchPost })(PostsShow);
+export default connect(mapStateToProps,{ fetchPost })(PostsShow);
