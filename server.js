4c75fac833c5c4eb049944384c359ff41c628aba
Franco <valterpassion@hotmail.com>
2017-12-26 22:07:05 -0200

    updates

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.static(__dirname));
-app.get('*', (req,res) =>{
+app.get('*', (req,res) => {
     res.sendFile(path.resolve(__dirname,'index.html'))
 });
 
